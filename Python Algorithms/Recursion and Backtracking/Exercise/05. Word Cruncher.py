27c647d30f6c35b600552c8d4720bd7dd3b90ac9
a-shiro <alekmitov20@gmail.com>
2022-07-19 13:53:21 +0300

    Modified Exercise

--- Python Algorithms/Recursion and Backtracking/Exercise/05. Word Cruncher.py
+++ Python Algorithms/Recursion and Backtracking/Exercise/05. Word Cruncher.py
@@ -1,28 +1,57 @@
-def find_combinations(combination, word, elements):
-    if ''.join(combination) == word:
-        print(*combination)
+def find_all_combinations(idx, target_word, words_count, words_by_idx, used_words):
+    if idx >= len(target_word):
+        print(' '.join(used_words))
         return
 
-    if ''.join(combination) not in word:
-        combination.clear()
+    if idx not in words_by_idx:
         return
 
-    for el1 in elements:
-        for el2 in elements:
-            combination.append(el1)
-            combination.append(el2)
+    for word in words_by_idx[idx]:
+        if words_count[word] == 0:
+            continue
 
-            find_combinations(combination, word, elements)
+        used_words.append(word)
+        words_count[word] -= 1
 
+        find_all_combinations(idx + len(word), target_word, words_count, words_by_idx, used_words)
 
-
-
-
-elements = input().split(', ')
-word = input()
-
-find_combinations([], word, elements)
+        used_words.pop()
+        words_count[word] += 1
 
 
-# me, so, do, m, ran
+words = input().split(', ')
+target_word = input()
+
+words_count = {}
+words_by_idx = {}
+
+for word in words:
+    if word in words_count:
+        words_count[word] += 1
+        continue
+
+    words_count[word] = 1
+
+    try:
+        idx = 0
+        while True:
+            idx = target_word.index(word, idx)
+
+            if idx not in words_by_idx:
+                words_by_idx[idx] = []
+            words_by_idx[idx].append(word)
+
+            idx += len(word)
+    except ValueError:
+        pass
+
+find_all_combinations(0, target_word, words_count, words_by_idx, [])
+
+# text, me, so, do, m, ran
 # somerandomtext
+
+# Word, cruncher, cr, h, unch, c, r, un, ch, er
+# Wordcruncher
+
+# tu, stu, p, i, d, pi, pid, s, pi
+# stupid
