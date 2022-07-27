906483c0ffa3fcbe3a76a2dbf82d2ad48bc0a7b6
a-shiro <alekmitov20@gmail.com>
2022-07-27 11:36:41 +0300

    Added Exercises

--- Python Algorithms/Searching And Sorting Algorithms/02. Selection Sort.py
+++ Python Algorithms/Searching And Sorting Algorithms/02. Selection Sort.py
@@ -1,9 +1,10 @@
 numbers = [int(n) for n in input().split()]
 
-for idx in range(len(numbers)):
+for idx, n in enumerate(numbers):
     min_number = min(numbers[idx:])
     min_number_idx = numbers.index(min_number, idx)
 
-    numbers[idx], numbers[min_number_idx] = numbers[min_number_idx], numbers[idx]
+    if min_number < n:
+        numbers[idx], numbers[min_number_idx] = numbers[min_number_idx], numbers[idx]
 
 print(*numbers)
