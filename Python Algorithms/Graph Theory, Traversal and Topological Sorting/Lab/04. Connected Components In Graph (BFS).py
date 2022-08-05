2450316fbd5e29c9bef88a788730b2a97fe7431c
a-shiro <alekmitov20@gmail.com>
2022-08-05 12:43:05 +0300

    Refactored Code

--- Python Algorithms/Graph Theory, Traversal and Topological Sorting/Lab/04. Connected Components In Graph (BFS).py
+++ Python Algorithms/Graph Theory, Traversal and Topological Sorting/Lab/04. Connected Components In Graph (BFS).py
@@ -16,26 +16,32 @@ def bfs(node, graph, visited, component):
                 component.append(child)
 
 
-graph = [
-    [3, 6],
-    [3, 4, 5, 6],
-    [8],
-    [0, 1, 5],
-    [1, 6],
-    [1, 3],
-    [0, 1, 4],
-    [],
-    [2]
-]
+nodes = int(input())
+graph = []
+
+for node in range(nodes):
+    line = input()
+    children = [] if line == '' else [int(x) for x in line.split()]
+    graph.append(children)
 
 visited = [False] * len(graph)
-component = []
 
 for node in range(len(graph)):
     if visited[node]:
         continue
 
+    component = []
     bfs(node, graph, visited, component)
 
-    print(*component)
-    component.clear()
+    print(f"Connected component: {' '.join([(str(x)) for x in component])}")
+
+# 9
+# 3 6
+# 3 4 5 6
+# 8
+# 0 1 5
+# 1 6
+# 1 3
+# 0 1 4
+#
+# 2
